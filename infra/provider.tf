4718e54caed1de6f87eb4434f2aef5100bfabd69
ravibharadwaj973 <jharavi0605@gmail.com>
2026-06-22 02:18:20 +0530

    again make git

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,13 +7,8 @@ terraform {
     }
   }
 
-  # Remote backend is required for running Terraform in GitHub Actions
-  # Replace 'YOUR_UNIQUE_S3_BUCKET_NAME' with a bucket you have created in AWS.
-  backend "s3" {
-    bucket         = "stickynoted-terraform-state-bucket"
-    key            = "state/terraform.tfstate"
-    region         = "us-east-1"
-  }
+  # Backend configuration is passed dynamically via -backend-config during 'terraform init'
+  backend "s3" {}
 }
 
 provider "aws" {
